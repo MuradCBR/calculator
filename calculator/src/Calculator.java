7477760f1663c15b0ff76fdd3d8441257e071c19
MuradCBR <murad.cabarov@gmail.com>
2022-12-20 21:15:31 +0400

    calculator methods
    Calculator iş prinsipi

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public float plus(float a,float b){
+        return a+b;
+    }
+    public float minus(float a,float b){
+        return a-b;
+    }
+    public float multiply(float a,float b){
+        return a+b;
+    }
+    public float divide(float a,float b){
+        return a/b;
+    }
+}
---
+++ calculator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       Calculator calculator=new Calculator();
+       float test1= calculator.divide(3,6);
+       float test2= calculator.multiply(3,6);
+
+System.out.println(test1);
+System.out.println(test2);
+
+    }
+}
